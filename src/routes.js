168959b217e6a25c8cbadb349bffd27426ca8b3e
mido <mido.ghanim@gmail.com>
2017-12-12 15:53:42 -0700

    comoplted step 7

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {Switch, Route} from  'react-router-dom'
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Student from './components/Student/Student'
+import ClassList from './components/ClassList/ClassList'
 
 export default (
 <Switch>
     <Route exact path="/" component = {Home}/>
     <Route  path="/about" component = {About}/>
+    <Route  path="/student/:id" component = {Student}/>
+    <Route  path="/classlist/:class" component = {ClassList}/>
 </Switch>
 
 )
